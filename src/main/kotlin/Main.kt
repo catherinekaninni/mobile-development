63518c8743d0bf552b02bc9d652c762fadc16625
catherinekaninni <catherinekaninni@gmail.com>
2022-02-12 10:05:34 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main() {
+    var name ="catherine kanini"
+    println(name)
+    var age= 20
+    println(age)
+    var phonenumber: String ="0748383345"
+    println(phonenumber)
+    var weight= 60
+    println(weight)
+    val kenyan:Boolean =true
+    println(kenyan)
+}
